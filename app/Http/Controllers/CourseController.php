7e777c3f18cc1632e34c7d75cd3fc4e523d3f4b9
System Administrator <root@Maury-MacBook-Pro.local>
2021-05-21 09:50:49 +0100

    Online courses management System with Laravel 8

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function index(){
+        $courses = Course::with('user')->withCount('episodes')->get();
+        return Inertia::render('Courses/index', compact('courses'));
+    } 
+
+    public function show(int $id){
+        $course = Course::where('id',$id)->with('episodes')->first();
+
+    return Inertia::render('Courses/show', compact('course'));
+    }
+}
---
+++ resources/js/Pages/Courses/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <app-layout>
+        <template #header>
+            Liste des formations
+        </template>
+        <div class="py-2" v-for="course in this.courseList" v-bind:key="course.id">
+            <div class="mx-8 bg-white rounded shadow p-4">
+                <div class="text-sm text-gray-600">Mise en ligne par : {{ course.user.name }}</div>
+                <div class="flex justify-between items-center">
+                    <div class="text-2xl">{{ course.title }}</div>
+                    <div class="text-sm text-gray">{{course.episodes_count}} épisodes</div>
+                </div>
+                <div class="text-sm text-gray-600">{{ course.description }}</div>
+                <a :href="'/course/'+course.id" class="bg-indigo-500 text-white px-2 py-1 text-sm mt-3 inline-block rounded hover:bg-indigo-700">Voir la formation</a>
+            </div>
+        </div>
+    </app-layout>
+</template>
+<script>
+import AppLayout from './../../Layouts/AppLayout';
+
+export default {
+    components: {
+        AppLayout
+    },
+
+    props:['courses'],
+
+    data(){
+        return {
+            courseList : this.courses
+        }
+    },
+
+    mounted(){
+        console.log(this.courseList);
+    }
+}
+</script>
